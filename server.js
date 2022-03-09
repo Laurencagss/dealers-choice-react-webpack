3786a4854af5e5bccc8c15d309a3b43708609759
Lauren Cagnetta <laurenscolleges@gmail.com>
2022-03-08 20:29:44 -0500

    try this

---
+++ server.js
@@ -0,0 +1,23 @@
+const {syncAndSeed, db } = require('./db.js');
+const PORT = 3030;
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/', (req, res)=>{
+  res.sendFile(path.join(__dirname, './index.html'));
+}
+)
+
+const init = async()=>{
+    try {
+      await syncAndSeed();
+      await app.listen(PORT, ()=>{
+        console.log(`listening on port ${PORT}`)
+      })
+    }
+    catch(e){
+      console.log(e)
+    }
+  }
+  init();
